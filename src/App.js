7f6a58620c28c3199cd417058720d3f37073eaf9
Komyob Raufzoda <komyobraufzoda@MacBook-Pro-admin.local>
2025-04-23 18:35:53 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login    from './components/Login';
-import ScanPage from './components/ScanPage';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import ScanPage from "./components/ScanPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"           element={<Navigate to="/login" replace />} />
-        <Route path="/login"      element={<Login />} />
-        <Route path="/scan"       element={ <ProtectedRoute> <ScanPage /></ProtectedRoute>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/scan"
+        element={
+          <ProtectedRoute>
+            {" "}
+            <ScanPage />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
