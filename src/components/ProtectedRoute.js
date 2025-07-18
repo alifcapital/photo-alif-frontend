8a64b9ca5c3d62f89aa8e07e019d2cdcd88ffd07
Komyob Raufzoda <komyobraufzoda@MacBook-Pro-admin.local>
2025-07-18 14:13:56 +0500

    authroute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,22 @@
 // src/components/ProtectedRoute.js
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem("authToken");
   // если нет токена — перенаправляем на логин
   if (!token) {
     return <Navigate to="/login" replace />;
   }
   return children;
+}
+
+export function AuthRoute({ children }) {
+  const token = localStorage.getItem("authToken");
+  // если нет токена — перенаправляем на логин
+  if (!token) {
+    return children;
+  }
+
+  return <Navigate to="/scan" replace />;
 }
