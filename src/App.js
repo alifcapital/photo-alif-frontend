42ba363487a682d5f9570e0a708b5ded043a3e1a
Komyob Raufzoda <komyobraufzoda@MacBook-Pro-admin.local>
2025-07-18 14:13:01 +0500

    ff

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import ScanPage from "./components/ScanPage";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute, { AuthRoute } from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Navigate to="/scan" replace />} />
+      <Route
+        path="/login"
+        element={
+          <AuthRoute>
+            <Login />
+          </AuthRoute>
+        }
+      />
       <Route
         path="/scan"
         element={
           <ProtectedRoute>
-            {" "}
             <ScanPage />
           </ProtectedRoute>
         }
